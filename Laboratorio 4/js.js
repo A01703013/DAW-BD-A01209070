bb5979324f7542b3d29d808811f61dd867939c89
A01209070 <A01209070@itesm.mx>
2019-01-28 11:51:09 -0600

    Avanzar Laboratorio 4

---
+++ Laboratorio 4/js.js
@@ -0,0 +1,124 @@
+function pedirnumero() {
+	var numero=parseInt(prompt("Por favor ingresa el número que quieras."));
+	var body = document.getElementsByTagName("body")[0];
+  	var tabla   = document.createElement("table");
+   	var tblBody = document.createElement("tbody");
+
+	  for (var i = 1; i <=numero; i++) {
+	    var hilera = document.createElement("tr");
+	 	var celda = document.createElement("td");
+	    for (var j = 1; j <=3; j++) {
+	    	if (j==1) {
+	    		var celda1 = document.createElement("td");
+		     	var textoCelda = document.createTextNode(i);
+		     	celda1.appendChild(textoCelda);
+      			hilera.appendChild(celda1);
+	    	}else if (j==2) {
+	    		var celda2 = document.createElement("td");
+		   		var textoCelda = document.createTextNode(i*i);      		
+		    	celda2.appendChild(textoCelda);
+      			hilera.appendChild(celda2);
+	    	}else{
+	    		var celda3 = document.createElement("td");
+		   		var textoCelda = document.createTextNode(i*i*i);
+		   		celda3.appendChild(textoCelda);
+      			hilera.appendChild(celda3);
+	    	}  		
+		}
+		tblBody.appendChild(hilera);  
+	 }
+ 	tabla.appendChild(tblBody); 
+	body.appendChild(tabla);
+	tabla.setAttribute("border", "2");
+  	tabla.setAttribute("align", "center");
+  	alert("La tabla se muestra al final de la página. Desplazarse para abajo por favor.");
+}
+
+function pedirresultadosuma(){
+	let a=Math.floor(Math.random() * (1100 - 0));
+	let b=Math.floor(Math.random() * (1100 - 0));
+	let inicio=performance.now();
+	let numero=prompt("Por favor ingresa el resultado al sumar "+a+"+"+b+": ");
+	let fin=performance.now();
+	if (numero==a+b) {
+		alert("El resultado de la suma es correcto. Usted tardo "+(fin-inicio)/1000+" segundos en contestar");
+	}else{
+		alert("El resultado de la suma es incorrecto. Usted tardo "+(fin-inicio)/1000+" segundos en contestar")
+	}
+}
+
+function pedirarreglo(){
+	let numero=parseInt(prompt("Por favor ingresa la cantidad de números en el arreglo."));
+	let arreglo=new Array (numero);
+	arreglo[0]=numero;
+	for (var i = 1; i <=numero; i++) {
+		arreglo[i]=parseFloat(prompt("Por favor ingresa el número de su eleción."));
+	}
+	return arreglo;
+}
+
+function countnumbers(){
+	let arg=pedirarreglo();
+	numerosnegativos(arg,arg.length);
+}
+
+function numerosnegativos(arreglo, numero){
+	let negativos=0, positivos=0, ceros=0;
+	for (var i = 1; i <numero; i++) {
+		if (arreglo[i]<0) {
+			negativos++;
+		}else if (arreglo[i]>0) {
+			positivos++;
+		}else{
+			ceros++;
+		}
+	}
+	alert("Hay "+negativos+" número(s) negativos, "+positivos+" número(s) positivos y "+ceros+ " cero(s).");
+
+}
+
+function pedirarreglodearreglos(){
+	let numero=parseFloat(prompt("Por favor ingresa la cantidad de arreglos en el arreglo."));
+	let arreglo=new Array (numero);
+	for (var i = 0; i <numero; i++) {
+		arreglo[i]=pedirarreglo();
+	}
+	let argpromedio=promedioarreglos(arreglo,numero);
+	for (var i = 0; i <numero; i++) {
+		alert("El promedio del arreglo número "+(i+1)+" es "+argpromedio[i]+".");
+	}
+}
+
+function promedioarreglos(arreglo,numero){
+	let aux=0;
+	let promedio=0;
+	let argpromedio=new Array(numero);
+	for (var i = 0; i<numero; i++) {
+		for (var j = 1; j <=arreglo[0]; i++) {
+			aux=aux+parseFloat(arreglo[i][j]);
+		}
+		promedio=aux/arreglo[0];
+		argpromedio[i]= parseFloat(promedio);
+	}
+	return argpromedio;
+}
+
+function invertirnumero(){
+	let numero=  parseInt(prompt("Por favorintroduzca el número a invertir:"));
+	let resto=numero, invertido=0;
+	do {
+    	invertido = invertido * 10 + (resto % 10)
+    	resto = Math.floor(resto / 10)
+ 	}while ( resto > 0 )
+    alert("El número invertido es: "+invertido);
+}
+
+
+
+
+
+
+
+
+
+
